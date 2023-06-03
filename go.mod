0ab2c7479caac0f393a0ea18b428e9f1133c5334
nrdabbad <nourreddine.abbad@outlook.fr>
2023-06-03 18:18:01 +0200

    first setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module img-shearlet
+
+go 1.20
+
+require github.com/fxtlabs/date v0.0.0-20150819233934-d9ab6e2a88a9
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/fxtlabs/date"
+)
+
+func main() {
+	fmt.Println("c'est que le début")
+	fmt.Println(math.Abs(-2.0))
+	fmt.Println(date.Today())
+}
