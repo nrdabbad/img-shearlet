735715db48bf530f3f0c4ed071de87ad18980a28
nrdabbad <nourreddine.abbad@outlook.fr>
2023-06-03 18:42:39 +0200

    modif

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math"
-
-	"github.com/fxtlabs/date"
 )
 
 func main() {
 	fmt.Println("c'est que le début")
-	fmt.Println(math.Abs(-2.0))
-	fmt.Println(date.Today())
 }
